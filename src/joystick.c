d912938e31b2f2c8d2c4bbff7b0f7079d1256b1b
Mary Hal <maryhal@users.noreply.github.com>
2015-08-30 13:50:22 -0700

    joystick wrapper + history

---
+++ src/joystick.c
@@ -0,0 +1,72 @@
+#include "joystick.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <GLFW/glfw3.h>
+
+struct joystick_t* createJoystick(struct joystick_t* joystick, int joystickNum)
+{
+    if (joystick == NULL)
+    {
+        joystick = (struct joystick_t*) malloc(sizeof(struct joystick_t));
+    }
+
+    joystick->id = joystickNum;
+
+    if (!glfwJoystickPresent(joystick->id))
+    {
+        perror("Requested joystick ID not found.");
+    }
+
+    return joystick;
+}
+
+void destroyJoystick(struct joystick_t* joystick, bool freeMe)
+{
+    if (freeMe)
+        free(joystick);
+}
+
+void updateButtons(struct joystick_t* joystick)
+{
+    memcpy(joystick->prevButtons, joystick->buttons, 16);
+    memcpy(joystick->prevAxis, joystick->axis, 8);
+
+    int buttonCount = 0;
+    const unsigned char* buttons = glfwGetJoystickButtons(joystick->id, &buttonCount);
+
+    for (int i = 0; i < buttonCount; i++)
+    {
+        joystick->buttons[i] = buttons[i];
+    }
+
+    int axisCount = 0;
+    const float* axes = glfwGetJoystickAxes(joystick->id, &axisCount);
+
+    for (int i = 0; i < axisCount; i++)
+    {
+        joystick->axis[i] = axes[i];
+    }
+}
+
+unsigned char getButtonState(struct joystick_t* joystick, int buttonID)
+{
+    return joystick->buttons[buttonID];
+}
+
+float getAxisState(struct joystick_t* joystick, int axisID)
+{
+    return joystick->axis[axisID];
+}
+
+bool buttonChange(struct joystick_t* joystick, int buttonID)
+{
+    return joystick->buttons[buttonID] != joystick->prevButtons[buttonID];
+}
+
+bool axisChange(struct joystick_t* joystick, int axisID)
+{
+    return joystick->axis[axisID] != joystick->prevAxis[axisID];
+}
