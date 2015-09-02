f9e8f8f49df4f4a488534f1beb095fd59a702fa1
ioCx <sanfordlam9@gmail.com>
2015-09-01 23:19:28 -0700

    Basic hash table font data storage

--- src/font.h
+++ src/font.h
@@ -4,17 +4,37 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#include <uthash.h>
+#include <stb_truetype.h>
+
+struct chardata_t
+{
+        int id;
+        UT_hash_handle hh;
+
+        stbtt_packedchar pchar;
+};
+
 struct font_t
 {
         unsigned int texture;
         unsigned int textureWidth;
         unsigned int textureHeight;
-
-        /* stbtt_packedchar pdata[256*2]; */
 };
+
+extern struct chardata_t* dataHash;
+
+void addCharData(int codepoint, stbtt_packedchar pchar);
+struct chardata_t* getCharData(int codepoint);
+void deleteCharData();
 
 struct font_t* loadFont(struct font_t* font, const char* filename, float pixelHeight);
 void destroyFont(struct font_t* font, bool freeFont);
+
+void getPackedQuad(struct font_t* font, int codepoint,
+                   float* xpos, float* ypos,
+                   int align_to_integer,
+                   stbtt_aligned_quad* q);
 
 void drawString(struct font_t* font, float x, float y, char* string);
 
