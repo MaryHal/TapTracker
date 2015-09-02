3ae7017ea0d659199a592ac6fb2edb76aea7b301
MaryHal <maryhal@users.noreply.github.com>
2015-09-01 23:19:28 -0700

    Basic hash table font data storage

--- src/font.c
+++ src/font.c
@@ -2,14 +2,44 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #include <GLFW/glfw3.h>
 
 #include <stb_rect_pack.h>
-#include <stb_truetype.h>
 
 /* #define STB_IMAGE_WRITE_IMPLEMENTATION */
 /* #include <stb_image_write.h> */
+
+struct chardata_t* dataHash = NULL;
+
+void addCharData(int codepoint, stbtt_packedchar pchar)
+{
+    struct chardata_t* s = getCharData(codepoint);
+
+    if (s == NULL)
+    {
+        s = (struct chardata_t*)malloc(sizeof(struct chardata_t));
+        s->id = codepoint;
+        s->pchar = pchar;
+
+        HASH_ADD_INT(dataHash, id, s);  /* id: name of key field */
+    }
+}
+
+struct chardata_t* getCharData(int codepoint)
+{
+    struct chardata_t* s = NULL;
+
+    HASH_FIND_INT(dataHash, &codepoint, s);  /* s: output */
+    return s;
+}
+
+void deleteCharData(struct chardata_t* cdata)
+{
+    HASH_DEL(dataHash, cdata);  /* user: pointer to deletee */
+    free(cdata);
+}
 
 struct font_t* loadFont(struct font_t* font, const char* filename, float pixelHeight)
 {
@@ -19,6 +49,8 @@ struct font_t* loadFont(struct font_t* font, const char* filename, float pixelHe
     }
     font->textureWidth  = 512;
     font->textureHeight = 512;
+
+    /* font->dataHash = NULL; */
 
     FILE* ttf_file = fopen(filename, "rb");
 
@@ -43,14 +75,15 @@ struct font_t* loadFont(struct font_t* font, const char* filename, float pixelHe
             perror("stbtt_PackBegin error");
         }
 
-        stbtt_pack_range pr[2];
+        stbtt_packedchar pdata[256*2];
+        stbtt_pack_range pr[1];
 
-        pr[0].chardata_for_range = font->pdata;
+        pr[0].chardata_for_range = pdata;
         pr[0].first_unicode_codepoint_in_range = 32;
         pr[0].num_chars = 95;
         pr[0].font_size = STBTT_POINT_SIZE(pixelHeight);
 
-        pr[1].chardata_for_range = font->pdata+256;
+        pr[1].chardata_for_range = pdata+256;
         pr[1].first_unicode_codepoint_in_range = 0xa0;
         pr[1].num_chars = 0x100 - 0xa0;
         pr[1].font_size = STBTT_POINT_SIZE(pixelHeight);
@@ -64,6 +97,18 @@ struct font_t* loadFont(struct font_t* font, const char* filename, float pixelHe
 
         stbtt_PackEnd(&pc);
         /* stbi_write_png("fonttest3.png", font->textureWidth, font->textureHeight, 1, temp_bitmap, 0); */
+
+        // Move all rects to hash table.
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < pr[i].num_chars; j++)
+            {
+                int codepoint = pr[i].first_unicode_codepoint_in_range + j;
+                /* int codepoint = j; */
+
+                addCharData(codepoint, pdata[i * 256 + j]);
+            }
+        }
     }
 
     glGenTextures(1, &font->texture);
@@ -87,6 +132,38 @@ void destroyFont(struct font_t* font, bool freeFont)
         free(font);
 }
 
+void getPackedQuad(struct font_t* font, int codepoint,
+                   float* xpos, float* ypos,
+                   int align_to_integer,
+                   stbtt_aligned_quad* q)
+{
+    float ipw = 1.0f / font->textureWidth;
+    float iph = 1.0f / font->textureHeight;
+
+    stbtt_packedchar* b = &getCharData(codepoint)->pchar;
+
+    if (align_to_integer) {
+        float x = (float) ((int)floor((*xpos + b->xoff) + 0.5f));
+        float y = (float) ((int)floor((*ypos + b->yoff) + 0.5f));
+        q->x0 = x;
+        q->y0 = y;
+        q->x1 = x + b->xoff2 - b->xoff;
+        q->y1 = y + b->yoff2 - b->yoff;
+    } else {
+        q->x0 = *xpos + b->xoff;
+        q->y0 = *ypos + b->yoff;
+        q->x1 = *xpos + b->xoff2;
+        q->y1 = *ypos + b->yoff2;
+    }
+
+    q->s0 = b->x0 * ipw;
+    q->t0 = b->y0 * iph;
+    q->s1 = b->x1 * ipw;
+    q->t1 = b->y1 * iph;
+
+    *xpos += b->xadvance;
+}
+
 void drawString(struct font_t* font, float x, float y, char* string)
 {
     glEnable(GL_TEXTURE_2D);
@@ -98,11 +175,7 @@ void drawString(struct font_t* font, float x, float y, char* string)
         glBegin(GL_QUADS);
         {
             stbtt_aligned_quad q;
-            stbtt_GetPackedQuad(font->pdata,
-                                font->textureWidth, font->textureHeight,
-                                *string - ' ',
-                                &x, &y, &q,
-                                1); //1=opengl & d3d10+,0=d3d9
+            getPackedQuad(font, *string, &x, &y, 1, &q);
 
             glTexCoord2f(q.s0,q.t0); glVertex2f(q.x0,q.y0);
             glTexCoord2f(q.s1,q.t0); glVertex2f(q.x1,q.y0);
