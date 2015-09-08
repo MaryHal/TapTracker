e58ea539ed0d3d599f08207f8581db7d38ad099a
MaryHal <maryhal@users.noreply.github.com>
2015-09-08 02:57:33 -0700

    "Color themes"

---
+++ src/colortheme.h
@@ -0,0 +1,25 @@
+#ifndef COLORTHEME_H
+#define COLORTHEME_H
+
+extern float DARK_THEME[6][3];
+extern float LIGHT_THEME[6][3];
+
+extern float (*colortheme)[6][3];
+
+enum COLOR_INDEX
+{
+    COLOR_SINGLE = 0,
+    COLOR_DOUBLE = 1,
+    COLOR_TRIPLE = 2,
+    COLOR_TETRIS = 3,
+    COLOR_FOREGROUND = 4,
+    COLOR_BACKGROUND = 5
+};
+
+void setColorTheme(float (*theme)[6][3]);
+void setGLColor(unsigned int colorIndex, float alpha);
+void setGLClearColor();
+
+void hexToRGB(float* rgbOutput, unsigned int hex);
+
+#endif /* COLORTHEME_H */
