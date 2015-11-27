de5fab79687382080af8b7bcc689ee6fb36bfd85
ioCx <sanfordlam9@gmail.com>
2015-11-26 16:27:54 -0800

    Clarify int size for memory mapped file

--- src/tracker.h
+++ src/tracker.h
@@ -2,7 +2,8 @@
 #define TRACKER_H
 
 #include <stdbool.h>
+#include <stdint.h>
 
-bool runTracker(int* dataPtr, unsigned int width, unsigned int height);
+bool runTracker(int32_t* dataPtr, unsigned int width, unsigned int height);
 
 #endif /* TRACKER_H */
