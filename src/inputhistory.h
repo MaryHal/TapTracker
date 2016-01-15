0c8568dcb5f48ea5740be21cb4fc2071f3bbac26
MaryHal <maryhal@users.noreply.github.com>
2016-01-15 13:44:18 -0800

    Renamed history_t to input_history_t

---
+++ src/inputhistory.h
@@ -0,0 +1,52 @@
+#ifndef INPUTHISTORY_H
+#define INPUTHISTORY_H
+
+#include <stddef.h>
+#include <stdbool.h>
+
+#include "joystick.h"
+
+#define MAX_INPUT_STRING_LENGTH 10
+#define INPUT_HISTORY_LENGTH    10
+
+struct button_t
+{
+        int key;
+        bool held;
+};
+
+struct element_t
+{
+        struct button_t spectrum[MAX_INPUT_STRING_LENGTH];
+        size_t size;
+        int level;
+};
+
+struct input_history_t
+{
+        struct element_t data[INPUT_HISTORY_LENGTH];
+        int start;
+        int end;
+
+        struct button_t* heldButtons[BUTTON_COUNT];
+
+        struct button_t* lastLeft;
+        struct button_t* lastUp;
+        struct button_t* lastRight;
+        struct button_t* lastDown;
+};
+
+struct input_history_t* createInputHistory(struct input_history_t* inputhistory);
+void destroyInputHistory(struct input_history_t* inputhistory, bool freeMe);
+void resetInputHistory(struct input_history_t* inputhistory);
+
+void pushInputHistoryElement(struct input_history_t* inputhistory, int level);
+struct button_t* pushKey(struct input_history_t* inputhistory, int key);
+
+void popInputHistoryElement(struct input_history_t* inputhistory);
+
+void pushInputFromJoystick(struct input_history_t* inputhistory, struct joystick_t* joystick);
+
+unsigned int joystickButtonToSheetIndex(unsigned int button);
+
+#endif /* INPUTHISTORY_H */
