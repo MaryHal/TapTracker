2a5ea8ed64a79d75f27de56b0e22aed5b00f2171
MaryHal <maryhal@users.noreply.github.com>
2016-03-07 23:47:13 -0800

    TGM1 tracker proof of concept

---
+++ tgm1/tgm1_tracker.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import mmap
+
+import struct
+import time
+
+from os import popen
+
+DATA_BLOCK_SIZE = 2
+
+def unpack_mmap_block(mm, n):
+    """Decode the nth 2-byte long byte string from mapped memory. This works with
+both python 2 and python 3"""
+    return struct.unpack("@h", mm[n*DATA_BLOCK_SIZE:(n+1)*DATA_BLOCK_SIZE])[0]
+
+def main():
+    with open("/dev/shm/tgmj_data", "r+b") as f:
+        # The tap_state struct has 11 2-byte integers, so that's how many bytes
+        # we're mapping.
+        mm = mmap.mmap(f.fileno(), DATA_BLOCK_SIZE * 2)
+
+        prevLevel = level = 0
+        while True:
+            # # Let's read the current game level and current timer value:
+            # level = unpack_mmap_block(mm, 0)
+            # timer = unpack_mmap_block(mm, 1)
+
+            # In Python 3, we can do something as simple as:
+            prevLevel = level;
+            level = int(mm[0 * DATA_BLOCK_SIZE])
+
+            if prevLevel != level:
+                sectionLevel = level % 100
+                if sectionLevel >= 90 and sectionLevel < 96:
+                    popen("mpv urgent.wav")
+                elif sectionLevel > 96:
+                    popen("mpv panic.wav")
+
+            # Sleepy-time may be a good idea.
+            time.sleep(0.01)
+
+        mm.close()
+
+if __name__ == '__main__':
+    main()
